e63a3a797c8270bb9e1ec9f11e010f3f709feeb9
Zota0 <77154534+Zota0@users.noreply.github.com>
2023-12-14 18:48:12 +0100

    Add project files.

---
+++ Minecraft_clone.cpp
@@ -0,0 +1,12 @@
+﻿// Minecraft_clone.cpp : Defines the entry point for the application.
+//
+
+#include "Minecraft_clone.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
